368f8a60cd8d03634353a75cf94ebe628f565ff6
unknown <bktessema9@gmail.com>
2025-02-17 15:40:01 +0300

    update on folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import react
+import React from "react";
+// Import the Routes and Route components from react-router
+import { Routes, Route } from "react-router";
+// Import the page components
+import Home from "./markup/pages/Home";
+import Login from "./markup/pages/Login";
+import AddEmployee from "./markup/pages/admin/AddEmployee";
+import Unauthorized from "./markup/pages/Unauthorized";
+// Import the Orders and Customers components
+import Orders from "./markup/pages/admin/Orders";
+import Customers from "./markup/pages/admin/Customers";
+// Import the Employees component
+import Employees from "./markup/pages/admin/Employees";
+
+// Import the css files
+import "./assets/template_assets/css/bootstrap.css";
+import "./assets/template_assets/css/style.css";
+import "./assets/template_assets/css/responsive.css";
+import "./assets/template_assets/css/color.css";
+
+// Import the custom css file
+import "./assets/styles/custom.css";
+
+// Import the Header component
+import Header from "./markup/components/Header/Header";
+// Import the Footer component
+import Footer from "./markup/components/Footer/Footer";
+
+// Import the PrivateAuthRoute component
+import PrivateAuthRoute from "./markup/components/Auth/PrivateAuthRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        {/* // Add the Orders Route  */}
+        <Route
+          path="/admin/orders"
+          element={
+            <PrivateAuthRoute roles={[1, 2, 3]}>
+              <Orders />
+            </PrivateAuthRoute>
+          }
+        />
+        {/* // Add the Customers Route  */}
+        <Route
+          path="/admin/customers"
+          element={
+            <PrivateAuthRoute roles={[2, 3]}>
+              <Customers />
+            </PrivateAuthRoute>
+          }
+        />
+        {/* // Add the Employees Route  */}
+        <Route path="/admin/employees" element={<Employees />} />
+        <Route
+          path="/admin/add-employee"
+          element={
+            <PrivateAuthRoute roles={[3]}>
+              <AddEmployee />
+            </PrivateAuthRoute>
+          }
+        />
+        {/* 
+          Customers (/admin/customers) - managers and admins
+          Orders (/admin/orders) - Can be accessed by all employees
+          Add employee (/admin/add-employee) - admins only 
+            - Admin: 3 
+            - Manager: 2 
+            - Employee: 1 
+        */}
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
